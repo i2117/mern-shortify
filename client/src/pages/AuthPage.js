10f20407dd70bae6d205a5437a1b6df1436cdc09
ilya_unity <@gmail.com>
2021-04-12 20:18:05 +0300

    Init

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,104 @@
+import React, {useState, useEffect, useContext} from 'react'
+import { AuthContext } from '../context/AuthContext'
+import {useHttp} from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
+
+export const AuthPage = () => {
+  const auth = useContext(AuthContext)
+  const message = useMessage()
+  const {loading, error, request, clearError} = useHttp()
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  useEffect(() => {
+    message(error)
+    clearError()
+  }, [error, message, clearError])
+
+  useEffect(() => {
+    window.M.updateTextFields()
+  }, [])
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form})
+      message(data.message)
+    } catch (e) {
+
+    }
+  }
+
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form})
+      auth.login(data.token, data.userId)
+    } catch (e) {
+
+    }
+  }
+
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Shorten link</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Authorization</span>
+            <div>
+
+              <div className="input-field">
+                <input 
+                  placeholder="Enter e-mail" 
+                  id="email" 
+                  type="text" 
+                  className="yellow-input"
+                  name="email"
+                  value={form.email}
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input 
+                  placeholder="Enter password" 
+                  id="password" 
+                  type="text" 
+                  className="yellow-input" 
+                  name="password"
+                  value={form.password}
+                  onChange={changeHandler}
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+
+            </div>
+          </div>
+          <div className="card-action">
+            <button 
+              className="btn yellow darken-4" 
+              style={{marginRight: 10}}
+              onClick={loginHandler}
+              disabled={loading}
+            >
+              Enter
+            </button>
+            <button 
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ client/src/pages/LinksPage.js
@@ -0,0 +1,37 @@
+import React, {useState, useContext, useCallback, useEffect} from 'react'
+import { Loader } from '../components/Loader'
+import { LinksList } from '../components/LinksList'
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
+
+export const LinksPage = () => {
+  const [links, setLinks] = useState([])
+  const {loading, request} = useHttp()
+  const {token} = useContext(AuthContext)
+
+  const fetchLinks = useCallback(async () => {
+      try {
+        const fetched = await request('/api/link', 'GET', null, {
+          Authorization: `Bearer ${token}`
+        })
+        setLinks(fetched)
+      } catch (e) {
+
+      }
+    }, [token, request]
+  )
+
+  useEffect(() => {
+    fetchLinks()
+  }, [fetchLinks])
+
+  if (loading) {
+    return <Loader />
+  }
+
+  return (
+    <>
+      { !loading && <LinksList links={links} /> }
+    </>
+  )
+}
